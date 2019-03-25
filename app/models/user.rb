ff9991b020336802d653357ca4da5e68b5ed7329
EC2 Default User <ec2-user@ip-172-31-41-195.us-east-2.compute.internal>
2019-03-25 09:30:26 +0000

    Finish 2nd section

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :miccroposts
+  validates :name, presence: true    # 「FILL_IN」をコードに置き換えてください
+  validates :email, presence: true
 end
---
+++ test/system/miccroposts_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class MiccropostsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit miccroposts_url
+  #
+  #   assert_selector "h1", text: "Miccropost"
+  # end
+end
